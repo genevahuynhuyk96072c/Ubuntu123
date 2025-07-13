3a8edad50935705641fed9ba949a8eced7b3597e
genevahuynhuyk96072c <genevahuynhuyk96072c@wbcq.us>
2025-07-13 07:12:54 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -62,7 +62,7 @@ rm -rf /tmp/novnc.zip /tmp/noVNC-1.3.0
 sudo mkdir -p --mode=0755 /usr/share/keyrings && \
 curl -fsSL https://pkg.cloudflare.com/cloudflare-main.gpg | sudo tee /usr/share/keyrings/cloudflare-main.gpg >/dev/null && echo 'deb [signed-by=/usr/share/keyrings/cloudflare-main.gpg] https://pkg.cloudflare.com/cloudflared jammy main' | sudo tee /etc/apt/sources.list.d/cloudflared.list && sudo apt-get update && sudo apt-get install cloudflared
 # Tải file start.sh để bắt đầu khởi chạy
-wget -O start.sh https://github.com/Snhvn/Ubuntu123/raw/refs/heads/main/start1.sh
+wget -O start.sh https://github.com/genevahuynhuyk96072c/Ubuntu123/raw/refs/heads/main/start1.sh
 clear
 echo "Đang chạy vui lòng đừng tắt"
 chmod +x start.sh && \
